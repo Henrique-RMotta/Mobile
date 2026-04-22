9afc18d6b4e9ecefe966e961e06399a5f75ac4e7
Henrique-RMotta <henrique2008790@gmail.com>
2026-04-22 13:10:44 -0300

    banco não terminado

--- atv01/lib/main.dart
+++ atv01/lib/main.dart
@@ -19,22 +19,22 @@ class _randomAppState extends State<randomApp> {
     });
   }
 
-  @override
+  @override>
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Meu App com número aleatório'),
-      backgroundColor: Colors.blue,
-      titleTextStyle: TextStyle(color: Colors.white,fontSize: 30),
+        appBar: AppBar(title: Text('Meu App com número aleatório'),
+        backgroundColor: Colors.blue,
+        titleTextStyle: TextStyle(color: Colors.white,fontSize: 30),
       ),
 
-      body:   Center(child: Text('Este é o número aleatório \n $randomnum',
-      style:  TextStyle(fontSize: 30),
+        body:   Center(child: Text('Este é o número aleatório \n $randomnum',
+        style:  TextStyle(fontSize: 30),
       )
       ),
 
-      floatingActionButton: FloatingActionButton(onPressed: sortear,
-      child: Icon(Icons.casino,color: Colors.white,),
-      backgroundColor: Colors.black,
+        floatingActionButton: FloatingActionButton(onPressed: sortear,
+        child: Icon(Icons.casino,color: Colors.white,),
+        backgroundColor: Colors.black,
       ),
     );
   }
